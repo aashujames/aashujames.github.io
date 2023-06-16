bb48f3075a1e890c04eab661f06620506a95d701
Ashish Gupta <ashishgupt25@gmail.com>
2023-06-17 00:16:13 +0530

    update data.js

--- data.js
+++ data.js
@@ -1,89 +1,56 @@
 export const home = {
-    domain: "jayhawk.github.io",
-    name: "Anubhav",
-    subtitle: "A Freelance Full-stack Engineer from India.",
-    email: "anubhavthrilling24@gmail.com",
+    domain: "aashujames.github.io",
+    name: "Ashish",
+    subtitle: "A Freelance Frontend Engineer from India.",
+    email: "ashishgupt25@gmail.com",
     resumeLink:
-        "https://github.com/jayhawk24/developerFolio/blob/9b4c2257ec56b4121a4a82545755cd1f610f7659/Anubhav's%20Resume.pdf",
-    desc: "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks."
+        "https://drive.google.com/file/d/1mEFhi9lvC_2dso8nHsqs2vhxHPOEWW66/view?usp=sharing",
+    desc: "A passionate Frontend Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries."
 };
 
 export const socials = {
     // Only usernames.
-    twitter: "jayh4wk",
-    github: "jayhawk24",
-    instagram: "__jayhawk__"
+    twitter: "AshishK_Gupta95",
+    github: "aashujames",
+    instagram: ""
 };
 
 export const about = {
     aboutMe:
-        "🙏 Namaste, I am Anubhav Gupta from Varanasi, India I am a Full Stack Developer at Gigwalla. Apart from coding, there are a lot of activities I love to do like...",
-    hobbies: ["Basketball", "Chess", "Hiking", "Swimming"],
+        "🙏 Namaste, I am Ashish Gupta from Varanasi, India I am a Frontend Developer at Gigwalla. Apart from coding, there are a lot of activities I love to do like...",
+    hobbies: ["Basketball", "Chess", "Badminton"],
     quote: "Write code that changes Lives...",
     skills: [
-        "nextjs",
         "react",
-        "redux",
         "python",
-        "bash",
-        "cplusplus",
         "css3",
-        "sass",
         "typescript",
-        "docker",
-        "django",
         "html5",
         "git",
         "javascript",
         "jquery",
-        "mongodb",
-        "nodejs",
-        "php"
+        "nodejs"
     ],
-    tools: ["github", "photoshop", "vim", "visualstudio", "linux"]
+    tools: ["github", "vim", "visualstudio", "linux"]
 };
 
 export const projects = [
     {
-        title: "Crispy",
-        imgSrc: "assets/crispy.png",
-        desc: "Shorten links blazingly fast, Fully serverless.",
-        link: "https://github.com/jayhawk24/crispy"
+        title: "Booknaav",
+        imgSrc: "assets/Screenshot_booknaav.png",
+        desc: "An online boat booking app where you can book boats of any type.",
+        link: "https://app.booknaav.com/"
     },
     {
-        title: "V80 Stocks",
-        imgSrc: "assets/v80.jpg",
-        desc: "List of stocks below 200 DMA which are in Vivek Singhal's v40 and v40 next stocks list.",
-        link: "https://github.com/jayhawk24/v80"
+        title: "Shopping Cart",
+        imgSrc: "assets/cartimage.png",
+        desc: "A shopping cart where you can add items to cart and can modify cart easily",
+        link: "https://github.com/aashujames/shopping-cart"
     },
     {
-        title: "E Store 24",
-        imgSrc: "assets/estore.png",
-        desc: "The website resembles a real store and you can add products to your cart and pay for them.",
-        link: "https://github.com/jayhawk24/E-store"
-    },
-    {
-        title: "Blood Bank",
-        imgSrc: "assets/donation.jpg",
-        desc: "Web application for hospitals to manage their blood bank and users who need blood. Made with PHP and MySQL.",
-        link: "https://github.com/jayhawk24/blood_bank"
-    },
-    {
-        title: "Ora Studio",
-        imgSrc: "assets/orastudio.png",
-        desc: "Architect Organisation Website. It is astatic website but with a great userinterface and you can check it out.",
-        link: "https://github.com/jayhawk24/orastudio"
-    },
-    {
-        title: "Zen Social",
-        imgSrc: "assets/zensocial.png",
-        desc: "Social App made with django. You can post your thoughts and ideas anonymously and also create groups and post there.",
-        link: "https://github.com/jayhawk24/zensocial"
-    },
-    {
-        title: "Take Turns",
-        imgSrc: "assets/takeTurns.png",
-        desc: "Easy Friendy Management, register your roommates, and it will simply show who's turn it is to carry out a task job.",
-        link: "https://github.com/jayhawk24/take-turns"
+        title: "Weather Data",
+        imgSrc: "assets/Screenshot_weather.png",
+        desc: "Displaying the weather information of a particular city",
+        link: "https://github.com/aashujames/WeatherDataApp"
     }
 ];
